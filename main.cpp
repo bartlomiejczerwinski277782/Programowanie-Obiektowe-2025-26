9ef75f188f5ab3b403acf35d575891af00dca2ac
Bartłomiej Czerwiński <277782@student.pwr.edu.pl>
2025-03-18 18:11:29 +0100

    Zadanie cząstkowe Kalkulator
    Zrobienie kalkulatora w c++ w zastosowaniu funkcji

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+double mem;
+bool mem_used;
+double add(double a, double b){
+    mem=a+b;
+    return mem;
+}
+double sub(double a, double b){
+    mem=a-b;
+    return mem;
+}
+double tim(double a, double b){
+    mem=a*b;
+    return mem;
+}
+double div(double a, double b){
+    mem=a/b;
+    return mem;
+}
+double mod(double a, double b){
+    return a-(int)(a/b)*b;
+}
+
+void erase(){
+
+}
+void menu(){
+    switch (add,sub,tim,div,mod) {
+        case add:{
+
+
+        }
+        case sub:{
+
+        }
+        case
+    }
+}
+int main() {
+
+    void(menu);
+
+}
